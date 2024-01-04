6433122dfe375dc59ec960b35f625c259bc16663
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-01-04 18:26:13 +0100

    Add lectura de datos y gestor que carga datos

---
+++ src/project/hospital-gui-wpf/src/dominio/Gestor.cs
@@ -0,0 +1,24 @@
+﻿using hospital_gui_wpf.src.persistencia;
+using System.Collections.Generic;
+
+namespace hospital_gui_wpf.src.dominio
+{
+	internal class Gestor
+	{
+		public List<Paciente> Pacientes;
+		public List<Personal> Personal;
+		public List<Usuario> Usuarios;
+
+		public Gestor()
+		{
+			// el uso de literales abstrae de si ha una base de datos o no xdd
+			// aunque es un churro, pero bueno, no se nos pide complicarnos
+			Usuarios = DataReader.CargarUsuariosXML("datos/usuarios.xml");
+			Pacientes = DataReader.CargarPacientesXML("datos/pacientes.xml");
+			Personal = DataReader.CargarPersonalXML("datos/personal.xml");
+
+			DataReader.CargarCitasXML("datos/citas.xml", Pacientes, Personal);
+			DataReader.CargarHistorialXML("datos/historial.xml", Pacientes);
+		}
+	}
+}
---
+++ src/project/hospital-gui-wpf/src/persistencia/DataReader.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using hospital_gui_wpf.src.dominio;
+
+namespace hospital_gui_wpf.src.persistencia
+{
+	internal class DataReader
+	{
+		public static List<Paciente> CargarPacientesXML(string filename)
+		{
+			List<Paciente> listado = new List<Paciente>();
+			int id;
+			string nombre;
+			string apellido;
+			DateTime fechaNacimiento;
+			int telefono;
+			string direccion;
+			Genero genero;
+			Uri imagen;
+			string correo;
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(filename);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				id = Convert.ToInt32(node["Id"].InnerText);
+				nombre = node["Nombre"].InnerText;
+				apellido = node["Apellido"].InnerText;
+				fechaNacimiento = DateTime.ParseExact(node["FechaNacimiento"].InnerText, "yyyy/MM/dd", null);
+				telefono = Convert.ToInt32(node["Telefono"].InnerText);
+				direccion = node["Direccion"].InnerText;
+				genero = (Genero)Enum.Parse(typeof(Genero), node["Genero"].InnerText, true);
+				imagen = new Uri(node["Imagen"].InnerText, UriKind.Relative);
+				correo = node["Correo"].InnerText;
+
+				listado.Add(new Paciente(id, nombre, apellido, fechaNacimiento, telefono, direccion, genero, imagen, correo, new List<Cita>(), new List<Historial>()));
+			}
+			return listado;
+		}
+
+		public static List<Personal> CargarPersonalXML(string filename)
+		{
+			List<Personal> listado = new List<Personal>();
+			int id;
+			string nombre;
+			string apellido;
+			DateTime fechaNacimiento;
+			int telefono;
+			string direccion;
+			Genero genero;
+			Uri imagen;
+			string correo;
+			TipoPersonal tipoPersonal;
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(filename);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				id = Convert.ToInt32(node["Id"].InnerText);
+				nombre = node["Nombre"].InnerText;
+				apellido = node["Apellido"].InnerText;
+				fechaNacimiento = DateTime.ParseExact(node["FechaNacimiento"].InnerText, "yyyy/MM/dd", null);
+				telefono = Convert.ToInt32(node["Telefono"].InnerText);
+				direccion = node["Direccion"].InnerText;
+				genero = (Genero)Enum.Parse(typeof(Genero), node["Genero"].InnerText, true);
+				imagen = new Uri(node["Imagen"].InnerText, UriKind.Relative);
+				correo = node["Correo"].InnerText;
+				tipoPersonal = (TipoPersonal)Enum.Parse(typeof(TipoPersonal), node["TipoPersonal"].InnerText, true);
+
+				listado.Add(new Personal(id, nombre, apellido, fechaNacimiento, telefono, direccion, genero, imagen, correo, tipoPersonal, new List<Cita>()));
+			}
+			return listado;
+		}
+
+		public static void CargarHistorialXML(string filename, List<Paciente> pacientes)
+		{
+			Paciente paciente;
+			string dolencias;
+			string tratamientos;
+			DateTime fechaAtencion;
+			Uri foto;
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(filename);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				paciente = pacientes.Find(p => p.Id == Convert.ToInt32(node["Paciente"].InnerText));
+
+				dolencias = node["Dolencias"].InnerText;
+				tratamientos = node["Tratamientos"].InnerText;
+				fechaAtencion = DateTime.ParseExact(node["FechaAtencion"].InnerText, "yyyy/MM/dd", null);
+				foto = new Uri(node["Foto"].InnerText, UriKind.Relative);
+
+				paciente.Historiales.Add(new Historial(dolencias, tratamientos, fechaAtencion, foto));
+			}
+		}
+
+		public static void CargarCitasXML(string filename, List<Paciente> pacientes, List<Personal> personal)
+		{
+			DateTime fechaCita;
+			TimeSpan duracion;
+			Paciente paciente;
+			Personal medico;
+			string info;
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(filename);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				fechaCita = DateTime.ParseExact(node["FechaCita"].InnerText, "yyyy/MM/dd HH:mm:ss", null);
+				duracion = TimeSpan.ParseExact(node["Duracion"].InnerText, "HH:mm:ss", null);
+
+				paciente = pacientes.Find(p => p.Id == Convert.ToInt32(node["Paciente"].InnerText));
+				medico = personal.Find(p => p.Id == Convert.ToInt32(node["Medico"].InnerText));
+
+				info = node["InfoAdicional"].InnerText;
+
+				paciente.Citas.Add(new Cita(fechaCita, duracion, paciente, medico, info));
+			}
+		}
+
+		public static List<Usuario> CargarUsuariosXML(string filename)
+		{
+			List<Usuario> listado = new List<Usuario>();
+			string nombreUsuario;
+			string nombre;
+			string apellido;
+			string contrasena;
+			DateTime fechaContratacion;
+			Uri imagen;
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(filename);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				nombreUsuario = node["NombreUsuario"].InnerText;
+				nombre = node["Nombre"].InnerText;
+				apellido = node["Apellido"].InnerText;
+				contrasena = node["Contrasena"].InnerText;
+				fechaContratacion = DateTime.ParseExact(node["FechaContratacion"].InnerText, "yyyy/MM/dd", null);
+				imagen = new Uri(node["Imagen"].InnerText, UriKind.Relative);
+
+				listado.Add(new Usuario(nombreUsuario, nombre, apellido, contrasena, fechaContratacion, imagen));
+			}
+			return listado;
+		}
+	}
+}
