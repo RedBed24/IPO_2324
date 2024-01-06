17cde7e1d5e89822db2b973811de3ec4da590abf
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-01-06 13:33:27 +0100

    CHG Gestor.Sanitarios U Gestor.Limpieza = Gestor.Personal

--- src/project/hospital-gui-wpf/src/dominio/Gestor.cs
+++ src/project/hospital-gui-wpf/src/dominio/Gestor.cs
@@ -6,7 +6,8 @@ namespace hospital_gui_wpf.src.dominio
 	public class Gestor
 	{
 		public List<Paciente> Pacientes;
-		public List<Personal> Personal;
+		public List<Personal> Limpieza;
+		public List<Personal> Sanitarios;
 		public List<Usuario> Usuarios;
 
 		public Gestor()
@@ -15,9 +16,12 @@ namespace hospital_gui_wpf.src.dominio
 			// aunque es un churro, pero bueno, no se nos pide complicarnos
 			Usuarios = DataReader.CargarUsuariosXML("datos/usuarios.xml");
 			Pacientes = DataReader.CargarPacientesXML("datos/pacientes.xml");
-			Personal = DataReader.CargarPersonalXML("datos/personal.xml");
+			List<Personal> Personal = DataReader.CargarPersonalXML("datos/personal.xml");
 
-			DataReader.CargarCitasXML("datos/citas.xml", Pacientes, Personal);
+			Limpieza = Personal.FindAll(p => p.Tipo == TipoPersonal.Limpieza);
+			Sanitarios =  Personal.FindAll(p => p.Tipo == TipoPersonal.Sanitario);
+
+			DataReader.CargarCitasXML("datos/citas.xml", Pacientes, Sanitarios);
 			DataReader.CargarHistorialXML("datos/historial.xml", Pacientes);
 		}
 	}
